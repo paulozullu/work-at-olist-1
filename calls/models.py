7bb2f6ad90bc3f3be05617e27c9697d56b7f61a6
Paulo Santos <paulozullu@gmail.com>
2018-07-10 13:36:35 -0300

    Changed PhoneCall model destination and source field to ForeignKey

--- calls/models.py
+++ calls/models.py
@@ -1,5 +1,6 @@
-from django.core.validators import RegexValidator
 from django.db import models
+
+from telephone_numbers.models import TelephoneNumber
 
 CALL_TYPES = [
     ("start", "start"),
@@ -14,17 +15,13 @@ class PhoneCall(models.Model):
     type = models.CharField(max_length=5, choices=CALL_TYPES)
     timestamp = models.DateTimeField()
     call_id = models.IntegerField()
-    phone_regex = RegexValidator(
-        regex=r'^\+?1?\d{10,11}$',
-        message='Phone number must be entered in the format: "9999999999".'
-                ' The first two digits are the area code.'
-                ' The phone number is composed of 8 or 9 digits.'
+    source = models.ForeignKey(
+        TelephoneNumber, related_name='source_number',
+        on_delete=models.PROTECT, null=True, default=None
     )
-    source = models.CharField(
-        validators=[phone_regex], max_length=11, null=True, default=None
-    )
-    destination = models.CharField(
-        validators=[phone_regex], max_length=11, null=True, default=None
+    destination = models.ForeignKey(
+        TelephoneNumber, related_name='destination_number',
+        on_delete=models.PROTECT, null=True, default=None
     )
 
     def __str__(self):
