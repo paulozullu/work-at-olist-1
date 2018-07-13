1c3f29cbd5490bff5798e5955ebaaa9300fc96df
Paulo Santos <paulozullu@gmail.com>
2018-07-13 01:29:55 -0300

    Disabled TZ use.

--- work_at_olist1/settings.py
+++ work_at_olist1/settings.py
@@ -109,13 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Bahia'
-
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
