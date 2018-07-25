4a4f202362c4225a10a6cff72a6d7df58146cc31
Paulo Santos <paulozullu@gmail.com>
2018-07-25 11:36:46 -0300

    Created test for TelephoneNumber model.

---
+++ telephone_numbers/tests/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from telephone_numbers.models import TelephoneNumber as TelephoneNumberModel
+
+
+class TelephoneNumber(TestCase):
+    """
+    Test module for TelephoneNumber model
+    """
+
+    def setUp(self):
+        TelephoneNumberModel.objects.create(
+            phone_number='123456789'
+        )
+        TelephoneNumberModel.objects.create(
+            phone_number='123456780'
+        )
+
+    def test_created(self):
+        """
+        Telephone numbers are created
+        """
+
+        number_123456789 = TelephoneNumberModel.objects.get(phone_number='123456789')
+        number_123456780 = TelephoneNumberModel.objects.get(phone_number='123456780')
+        self.assertIsNotNone(number_123456789)
+        self.assertIsNotNone(number_123456780)
