2a3ba1d55bb4f72a82b91f7a12277dd6080e6490
Paulo Santos <paulozullu@gmail.com>
2018-07-25 12:11:09 -0300

    Created test for PhoneCall and Bill

--- calls/tests/tests.py
+++ calls/tests/tests.py
@@ -1,3 +1,52 @@
+from datetime import datetime, timezone
+
 from django.test import TestCase
 
-# Create your tests here.
+from calls.models import PhoneCall, Bill
+from telephone_numbers.models import TelephoneNumber
+
+
+class PhoneCallTestCase(TestCase):
+    """
+    Test module for Call model
+    """
+
+    def setUp(self):
+        source = TelephoneNumber.objects.create(
+            phone_number='123456789'
+        )
+        destination = TelephoneNumber.objects.create(
+            phone_number='123456780'
+        )
+
+        PhoneCall.objects.create(
+            type=PhoneCall.CallTypes.start.value,
+            source=source,
+            destination=destination,
+            timestamp=datetime.now(timezone.utc),
+            call_id=12345
+        )
+
+        PhoneCall.objects.create(
+            type=PhoneCall.CallTypes.end.value,
+            timestamp=datetime.now(timezone.utc),
+            call_id=12345
+        )
+
+    def test_created_phone_call_bill(self):
+        """
+        Test whether the PhoneCalls and the Bill were created
+        """
+        start_call = PhoneCall.objects.get(
+            type=PhoneCall.CallTypes.start.value,
+            call_id=12345
+        )
+        end_call = PhoneCall.objects.get(
+            type=PhoneCall.CallTypes.end.value,
+            call_id=12345
+        )
+        bill = Bill.objects.get(call_id=12345)
+
+        self.assertIsNotNone(start_call)
+        self.assertIsNotNone(end_call)
+        self.assertIsNotNone(bill)
