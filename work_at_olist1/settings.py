7c6ecfbe7fd33a113b5cef3b8bcf3cd24dd4e29a
Paulo Santos <paulozullu@gmail.com>
2018-07-30 10:26:53 -0300

    Setup to deploy to Heroku

--- work_at_olist1/settings.py
+++ work_at_olist1/settings.py
@@ -12,21 +12,22 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from decouple import config
+from dj_database_url import parse as dburl
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@z)o34d&xis-k5izz*($uj*+e7s&brjl^h@k@s%&w-!pd=k&rv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,17 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'work_at_olist1.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -103,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -117,8 +111,13 @@ USE_TZ = True
 
 TIME_ZONE = 'UTC'
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = 'imagens'
+
+MEDIA_URL = '/media/'
--- work_at_olist1/urls.py
+++ work_at_olist1/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
@@ -42,8 +44,8 @@ router.register(r'calls', PhoneCallViewSet, base_name='PhoneCall')
 router.register(r'bills', BillViewSet, base_name='Bill')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    url(r'^docs/$', schema_view.with_ui(
-        'swagger', cache_timeout=None), name='docs'),
-]
+                  path('', include(router.urls)),
+                  path('admin/', admin.site.urls),
+                  url(r'^docs/$', schema_view.with_ui(
+                      'swagger', cache_timeout=None), name='docs'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
